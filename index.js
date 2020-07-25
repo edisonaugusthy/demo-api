96d60783635aca80e080cacfdfcb07ea83dfa5ed
Edison Augusthy <edisonaugusthy117@gmail.com>
2020-07-25 17:22:45 +0530

    initial design

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var cors = require('cors')
+
+const PORT = process.env.PORT || 5000;
+
+const content = `<div> no payment made</div>`
+app.use(cors())
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+app.get("/", (req, res, next) => {
+  res.json('server is up mahnn');
+});
+
+app.get("/status", (req, res, next) => {
+  res.json({ status: false, timeout: 5000, content: content });
+});
